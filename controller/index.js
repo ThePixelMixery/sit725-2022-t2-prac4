0e2d2d4f4e21ff8b748d0f82122daa8cd490ce73
Aya <pixelmixery@gmail.com>
2022-08-31 12:31:09 +1000

    Revert "Prac 5"
    This reverts commit 748dfdfa588d262faeb036b4fe31500adc1f4684.

--- controller/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-	projectController: require("./projectController"),
-	userController: require("./userController.js"),
-};
--- server.js
+++ server.js
@@ -2,22 +2,71 @@ require("dotenv").config();
 var express = require("express");
 var app = express();
 var cors = require("cors");
-let dbConnect = require("./dbConnect");
-let projectRoutes = require("./routes/projectRoute");
-let userRoute = require("./routes/userRoute");
-let http = require("http").createServer(app);
-
+const MongoClient = require("mongodb").MongoClient;
+let projectCollection;
 app.use(express.static(__dirname + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
-
-app.use("/api/projects", projectRoutes);
-
-app.use("/api/user", userRoute);
-
-var port = process.env.port || 5500;
-
+//connection
+const uri =
+	"mongodb+srv://" +
+	process.env.mongo_user +
+	":" +
+	process.env.mongo_password +
+	"@prac4.th6vaw0.mongodb.net/SIT725_2022_t1?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+var port = process.env.port || 3000;
+const cardList = [
+	{
+		title: "Pumpkin",
+		image: "images/dog3.jpg",
+		link: "About Pumpkin",
+		description: "Pumpkin is a puppy",
+	},
+	{
+		title: "Roxy",
+		image: "images/dog4.jpg",
+		link: "About Roxy",
+		description: "Roxy is a puppy",
+	},
+];
+app.get("/api/projects", (req, res) => {
+	res.json({ statusCode: 200, data: cardList, message: "Success" });
+});
+const createColllection = (collectionName) => {
+	client.connect((err, db) => {
+		projectCollection = client.db().collection(collectionName);
+		if (!err) {
+			console.log("MongoDB Connected");
+		} else {
+			console.log("DB Error: ", err);
+			process.exit(1);
+		}
+	});
+};
+const insertProjects = (project, callback) => {
+	projectCollection.insert(project, callback);
+};
+const getProjects = (callback) => {
+	projectCollection.find({}).toArray(callback);
+};
+app.post("/api/projects", (req, res) => {
+	console.log("New Project added", req.body);
+	var newProject = req.body;
+	insertProjects(newProject, (err, result) => {
+		if (err) {
+			res.json({ statusCode: 400, message: err });
+		} else {
+			res.json({
+				statusCode: 200,
+				message: "Project Successfully added",
+				data: result,
+			});
+		}
+	});
+});
 app.listen(port, () => {
 	console.log("App running at http://localhost:" + port);
+	createColllection("pets");
 });
